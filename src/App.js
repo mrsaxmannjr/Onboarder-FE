9323f82d64352da8b699140280b47ba06788dbcd
mrsaxmannjr <mrsaxmannjr@gmail.com>
2018-05-29 12:06:39 -0600

    created boilerplate

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Header from "./Components/Header";
+import Login from "./Components/Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Header />
+          <Switch>
+            <Route exact path="/Login" component={Login} />
+          </Switch>
+        </React.Fragment>
+      </Router>
     );
   }
 }
